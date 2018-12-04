b9e7415b4e4b7da6dcc3c6a3d5173d6b79eca7b4
TheSDTM <yaroslavperf@gmail.com>
2018-12-04 21:07:38 +0300

    Attempt to fix cors

--- webserver/middlewares/cors.go
+++ webserver/middlewares/cors.go
@@ -11,15 +11,20 @@ import (
 func Cors(next handlers.HandlerFunc) handlers.HandlerFunc {
 	return func(env *handlers.Env, w http.ResponseWriter, r *http.Request) error {
 		if origin := r.Header.Get("Origin"); origin != "" {
-			if strings.HasSuffix(origin, ".jackal.online") {
-				w.Header().Set("Access-Control-Allow-Origin", origin)
+			originSplitted := strings.Split(origin, ":")
+			if strings.HasSuffix(originSplitted[0], ".jackal.online") {
+				if len(originSplitted) > 1 {
+					w.Header().Set("Access-Control-Allow-Origin", originSplitted[0]+":"+originSplitted[1])
+				} else {
+					w.Header().Set("Access-Control-Allow-Origin", originSplitted[0])
+				}
 			} else {
 				w.Header().Set("Access-Control-Allow-Origin", "jackal.online")
 			}
 		}
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT")
 
 		if r.Method == http.MethodOptions {
 			return nil
